f92386d6e829acdbf2ef0d2e3bda14820deadfdd
cedric <149272209+cjeannoel2005@users.noreply.github.com>
2025-04-29 22:18:38 -0400

    Create homeController.java

---
+++ Creating the Project/cjeannoel2005 swe1-project main Creating the Project-ecommerce_ecommerce/src/main/java/com/TheDebuggers/ecommerce/controllers/homeController.java
@@ -0,0 +1,43 @@
+package com.TheDebuggers.ecommerce;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeControl {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/checkout")
+    public String checkoutPage() {
+        return "checkout";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+
+    @GetMapping("/shoppingCart")
+    public String shoppingCartPage() {
+        return "ShoppingCart";
+    }
+
+    @GetMapping("/adminPage")
+    public String adminPage() {
+        return "AdminPage";
+    }
+
+    @GetMapping("/bookTemplate")
+    public String bookTemplatePage() {
+        return "Book_Template";
+    }
+}
